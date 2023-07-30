02fe7921ab3da334aec2b360cd00b604aa1b1780
annad <annadiacova@gmail.com>
2023-07-31 01:39:04 +0300

    ДЗ 5

---
+++ task_3.py
@@ -0,0 +1,11 @@
+# Создайте функцию генератор чисел Фибоначчи
+
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+
+n = int(input('Введите число: '))
+print(*(fibonacci(n)))
